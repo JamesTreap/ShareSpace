ecc2d07274c0e096f000abe9c69a9bbb39e9defb
James Treap <james.treap@gmail.com>
2025-06-09 15:29:25 -0400

    Made signup return JWT token

--- flask_api/auth.py
+++ flask_api/auth.py
@@ -1,5 +1,6 @@
 import jwt
 from flask import Blueprint, request, jsonify, current_app
+from werkzeug.security import generate_password_hash, check_password_hash
 from database import users_collection
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -13,12 +14,20 @@ def create_user():
     if users_collection.find_one({"username": data["username"]}):
         return jsonify({'error': 'User already exists'}), 409
 
-    # TODO: Hash the password before storing
+    hashed_password = generate_password_hash(data["password"])
     users_collection.insert_one({
         "username": data["username"],
-        "password": data["password"]
+        "password": hashed_password
     })
-    return jsonify(f"Successfully created user with username {data['username']}"), 201
+
+    token = jwt.encode({
+        'username': data["username"]
+    }, current_app.config['SECRET_KEY'], algorithm='HS256')
+    
+    return jsonify({
+        "message": f"Successfully created user with username {data['username']}",
+        "token": token
+    }), 201
 
 
 @auth_bp.route('/login', methods=['POST'])
@@ -28,7 +37,10 @@ def login():
         return jsonify({'error': 'Invalid input'}), 400
 
     user = users_collection.find_one({"username": data["username"]})
-    if not user or user['password'] != data['password']:
+    if not user:
+        return jsonify({'error': 'Invalid username or password'}), 401
+    
+    if not check_password_hash(user['password'], data['password']):
         return jsonify({'error': 'Invalid username or password'}), 401
 
     token = jwt.encode({
